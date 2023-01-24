f0f4749c5365e8427346add9930ecf8293ece534
zhouxingyu-warwick <107267531+zhouxingyu-warwick@users.noreply.github.com>
2023-01-24 13:54:51 +0800

    Update NotionPage.tsx

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -185,12 +185,12 @@ export const NotionPage: React.FC<types.PageProps> = ({
   const keys = Object.keys(recordMap?.block || {})
   const block = recordMap?.block?.[keys[0]]?.value
 
-  // const isRootPage =
-  //   parsePageId(block?.id) === parsePageId(site?.rootNotionPageId)
+  const isRootPage =
+     parsePageId(block?.id) === parsePageId(site?.rootNotionPageId)
   const isBlogPost =
     block?.type === 'page' && block?.parent_table === 'collection'
 
-  const showTableOfContents = !!isBlogPost
+  const showTableOfContents = !!isBlogPost && !!isRootPage //change toc
   const minTableOfContentsItems = 3
 
   const pageAside = React.useMemo(
