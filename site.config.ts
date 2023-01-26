14487b04b811c505440964965da832225349e1be
zhouxingyu-warwick <107267531+zhouxingyu-warwick@users.noreply.github.com>
2023-01-26 15:53:23 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -51,15 +51,15 @@ export default siteConfig({
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
   navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
+   navigationStyle: 'custom',
+   navigationLinks: [
   //   {
   //     title: 'About',
   //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
   //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+     {
+       title: 'Contact',
+       pageId: '98d6d850be444ac783c8d9b8ae5d49a8'
+     }
+   ]
 })
