03ba7b1c8e3c7f0a870b3b31719a712677346598
Xingyu Zhou <124552361+xingyuzhouphd@users.noreply.github.com>
2023-03-04 23:28:54 +0800

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -61,7 +61,6 @@ export const youtube: string | null = getSiteConfig('youtube', null)
 export const linkedin: string | null = getSiteConfig('linkedin', null)
 export const newsletter: string | null = getSiteConfig('newsletter', null)
 export const zhihu: string | null = getSiteConfig('zhihu', null)
-export const googlescholar: string | null = getSiteConfig('googlescholar', null)
 
 export const getMastodonHandle = (): string | null => {
   if (!mastodon) {
